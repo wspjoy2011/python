9596ec9295c0a27a2c237a24d752aae04da71787
wspjoy2011 <47632657+wspjoy2011@users.noreply.github.com>
2021-04-11 21:22:47 +0300

    Add files via upload

---
+++ test_code/recursion.py
@@ -0,0 +1,37 @@
+menu1 = [
+    {
+        'label': 'Yii framework',
+        'url': 'https://yii.ru'
+    },
+    {
+        'label': 'More frameworks',
+        'items': [
+            {'label': 'Laravel', 'url': 'http://laravel.com',
+             'items': [
+                 {'label': 'Google', 'url': 'http://google.com'}
+             ]
+             },
+            {'label': 'Slim', 'url': 'http://slimframework.com'}
+        ]
+    },
+    {
+        'label': 'Symfony',
+        'url': 'http://symfony.com'
+    }
+]
+
+
+def build_menu(menu):
+    result = ''
+    for i in range(len(menu)):
+        for key, value in enumerate(menu[i]):
+            if value == 'label':
+                result += f'{value}: {menu[i][value]}\n'
+            elif value == 'url':
+                result += f'{value}: {menu[i][value]}\n\n'
+            elif value == 'items':
+                result += build_menu(menu[i][value])
+    return result
+
+
+print(build_menu(menu1))
