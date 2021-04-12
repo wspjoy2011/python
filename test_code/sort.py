143dbe0e686b38719af60063f64e6348358c0632
wspjoy2011 <47632657+wspjoy2011@users.noreply.github.com>
2021-04-12 15:55:07 +0300

    Create sort.py

---
+++ test_code/sort.py
@@ -0,0 +1,41 @@
+import random
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >=0 and key < arr[j] :
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+
+lst = [random.randint(1, 255) * x for x in range(20)]
+insertion_sort(lst)
+
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+
+
+lst = [random.randint(1, 255) * x for x in range(20)]
+selection_sort(lst)
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+lst = [random.randint(1, 255) * x for x in range(20)]
+bubble_sort(lst)
