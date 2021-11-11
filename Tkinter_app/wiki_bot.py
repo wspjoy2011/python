696c84b0053776bb24fec910e2819e27fb66900c
wspjoy2011 <47632657+wspjoy2011@users.noreply.github.com>
2021-11-11 14:58:06 +0200

    Add files via upload

---
+++ Tkinter_app/wiki_bot.py
@@ -0,0 +1,23 @@
+import wikipedia
+import telebot
+
+wikipedia.set_lang('ru')
+bot = telebot.TeleBot('2066259622:AAEi050kt5H8iV2A-i8xAVs88ehZyd8Urrs')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    sending_message = f'<b>Привет {message.from_user.first_name}!</b>\n' \
+                      f'Чтобы приступить к поиску Введите нужное слово и получите ответ.'
+    bot.send_message(message.chat.id, sending_message, parse_mode='html')
+
+
+@bot.message_handler(content_types=['text'])
+def mess(message):
+    final_message = ''
+    word = message.text.strip().lower()
+    final_message = wikipedia.summary(word)
+    bot.send_message(message.chat.id, final_message, parse_mode='html')
+
+
+bot.polling(none_stop=True)
