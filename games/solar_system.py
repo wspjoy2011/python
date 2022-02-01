451b9f2bb75bfc521cc3546859e08cebdbe5d2c6
wspjoy2011 <47632657+wspjoy2011@users.noreply.github.com>
2022-02-01 14:27:06 +0200

    Update solar_system.py

--- games/solar_system.py
+++ games/solar_system.py
@@ -9,8 +9,9 @@ SCREEN_HEIGHT = 950
 
 
 class Planet(Turtle):
-    def __init__(self, shapesize, planet_color, radius, star, increase_angle):
+    def __init__(self, shapesize, planet_color, radius, star, increase_angle, name='star'):
         Turtle.__init__(self, shape='circle')
+        self.name = name
         self.speed(0)
         self.shapesize(*shapesize)
         self.x = 0
@@ -41,6 +42,10 @@ class Asteroid(Planet):
 
 def move_objects(objects):
     for object in objects:
+        if object.name == 'saturn':
+            saturn_circle.down()
+            saturn_circle.goto(object.xcor(), object.ycor() - 50)
+            saturn_circle.circle(50)
         object.move()
 
 
@@ -58,7 +63,7 @@ sun.shapesize(5, 5)
 earth = Planet((1.0, 1.0), 'blue', 150, sun, 0.02)
 moon = Planet((0.5, 0.5), 'gray', 25, earth, 0.08)
 
-saturn = Planet((2.0, 2.0), 'orange', 200, sun, 0.005)
+saturn = Planet((2.0, 2.0), 'orange', 200, sun, 0.005, name='saturn')
 
 mars = Planet((0.8, 0.8), 'red', 300, sun, 0.007)
 phobos = Planet((0.3, 0.3), 'grey', 40, mars, 0.06)
@@ -67,13 +72,18 @@ deimos = Planet((0.2, 0.2), 'white', 25, mars, 0.08)
 planets = [earth, moon, saturn, mars, phobos, deimos]
 
 asteroids = [Asteroid(sun, randint(400, 600), 0.012421) for _ in range(500)]
-asteroids_saturn = [Asteroid(saturn, 40, 0.01) for _ in range(700)]
+
+saturn_circle = Turtle()
+saturn_circle.up()
+saturn_circle.hideturtle()
+saturn_circle.width(5)
+saturn_circle.speed(0)
+saturn_circle.color('white', 'white')
+
 
 window.listen()
 while True:
-    window.update()
+    saturn_circle.clear()
     move_objects(planets)
     move_objects(asteroids)
-    move_objects(asteroids_saturn)
-
-    # sleep(0.001)
+    window.update()
